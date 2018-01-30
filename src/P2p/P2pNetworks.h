e54a8e304958f6501d33fa7833d456b9e4e1c5b2
FogCryptoCoin <35573632+FogCryptoCoin@users.noreply.github.com>
2018-01-30 13:01:42 -0600

    Okay

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x81, 0xff, 0x30, 0x7c, 0x8c, 0xee, 0xd3, 0x8d, 0x3c, 0xf8, 0xcd, 0x48, 0x70, 0xd6, 0xb2, 0x07 } };
+}
